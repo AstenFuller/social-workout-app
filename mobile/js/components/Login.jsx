d769bbc9cfa034637d649529c942ed745accb87e
Eric Dalmau <dalmaueric29@gmail.com>
2020-03-25 14:12:43 -0700

    created login page layout

--- mobile/js/components/Login.jsx
+++ mobile/js/components/Login.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { TextInput, Text, View, TouchableOpacity } from 'react-native';
+import { Button, Icon } from 'react-native-elements';
+import {login} from '../../styles/Styles';
 
 export default class Login extends Component {
+
     render() {
         return (
-            <Text>Hi</Text>
+           <View style={login.container}>
+               <TextInput style={login.input} placeholder="Enter Email" placeholderTextColor='white' />
+               <TextInput style={login.input} placeholder="Enter Password" placeholderTextColor='white'/>
+               <TouchableOpacity style={login.button}>
+                   <Text style={login.buttonText}>Sign In</Text>
+               </TouchableOpacity>
+               <TouchableOpacity style={login.button}>
+                   <Text style={login.buttonText}>Register</Text>
+               </TouchableOpacity>     
+           </View>
         )
     }
 }
