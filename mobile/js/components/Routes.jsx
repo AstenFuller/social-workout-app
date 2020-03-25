40be66d995a8aeac7c7c6b53d451cdacdf146b81
Asten Fuller <astenhfuller@gmail.com>
2020-03-25 16:23:03 -0700

    email password checks against database and provides token upon login

--- mobile/js/components/Routes.jsx
+++ mobile/js/components/Routes.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Router, Scene, Stack } from 'react-native-router-flux';
-import Login from './Login';
+import Login from './LoginRegister/Login';
+import LandingPage from './Main/LandingPage'
 
 export default class Routes extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class Routes extends Component {
       <Router>
         <Stack key='root'>
           <Scene key='login' component={Login} initial={true} hideNavBar={true} />
+          <Scene key='landingPage' component ={LandingPage} hideNavBar={true} />
         </Stack>
       </Router>
     )
